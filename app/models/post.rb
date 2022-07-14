dd5687f8aceb81aa65f89a9afeaf02657a9312e0
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-07-14 20:51:56 +0530

    feat:TE5-T965, add rolify and implemented it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
   validates :tittle, presence: true
 
+  resourcify
+
+  has_many :users, -> { distinct }, through: :roles, class_name: "User", source: :users
+  has_many :creators, -> { where(roles: { name: :creator }) }, through: :roles, class_name: "User", source: :users
+  has_many :editors, -> { where(roles: { name: :editor }) }, through: :roles, class_name: "User", source: :users
+
   def to_s
     title
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :posts, through: :roles, source: :resource, source_type: :Post
+  has_many :creator_posts, -> { where(roles: { name: :creator }) }, through: :roles, source: :resource, source_type: :Post
+  has_many :editor_posts, -> { where(roles: { name: :editor }) }, through: :roles, source: :resource, source_type: :Post
+
+  after_create :assign_default_role
+  validate :must_have_a_role, on: :update
+
+  private
+    def assign_default_role
+      self.add_role(:newuser) if self.roles.blank?
+    end
+
+    def must_have_a_role
+      unless roles.any?
+        errors.add(:roles, "must have atleast one role")
+      end
+    end
 end
